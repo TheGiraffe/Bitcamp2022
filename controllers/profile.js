7231359363757b132e06cd013f8b7ef980fa154b
TheGiraffe <TheGiraffe@users.noreply.github.com>
2022-04-10 01:04:23 -0400

    fixed profile dashboard route issue

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,19 @@
 const Profile = require("../models/profile.js");
 const Skill = require("../models/skill.js");
 const Career = require("../models/career.js");
+
+function index(req, res){
+    Profile.find({})
+    .then(profiles => {
+        console.log(profiles)
+        res.render('dashboard/index',{
+            profiles,
+            title: "hi"
+        })
+    })
+    .catch(err => console.error(err))
+}
+
+module.exports = {
+    index
+}
